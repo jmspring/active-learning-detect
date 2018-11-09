5834054af666b5dfa22476374dc6f8e532bb3fb9
Amanda Kaufman <aka@microsoft.com>
2018-11-09 12:32:22 -0700

    Issue: Http trigger function produces Exception: OSError: 22 when attempting to copy file from temp blob storage to permanent storage.

--- functions/pipeline/onboarding/__init__.py
+++ functions/pipeline/onboarding/__init__.py
@@ -35,6 +35,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # Build list of image objects to pass to DAL for insertion into DB.
     image_object_list = []
     image_name_list = []
+    
+    # TODO: Add check to ensure image URLs sent by client are all unique.
+
     for url in url_list:
         # Split original image name from URL
         original_filename = url.split("/")[-1]
@@ -46,8 +49,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         image_object_list.append(image)
 
     # Get database connection
+    # Verbose logging for testing:
+    # logging.info("DB_HOST = " + os.getenv('DB_HOST'))
+    # logging.info("DB_NAME = " + os.getenv('DB_NAME'))
+    # logging.info("DB_USER = " + os.getenv('DB_USER'))
+    # logging.info("DB_PASS = " + os.getenv('DB_PASS'))
+
+    logging.info("Now connecting to database...")
     db_config = DB_Access_V2.DatabaseInfo(os.getenv('DB_HOST', default_db_host), os.getenv('DB_NAME', default_db_name), os.getenv('DB_USER', default_db_user), os.getenv('DB_PASS', default_db_pass))
     data_access = DB_Access_V2.ImageTagDataAccess(DB_Access_V2.PostGresProvider(db_config))
+    logging.info("Connected.")
 
     # Create user id
     user_id = data_access.create_user(DB_Access_V2.getpass.getuser())
@@ -56,21 +67,34 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # Add new images to the database, and retrieve a dictionary ImageId's mapped to ImageUrl's
     image_id_url_map = data_access.add_new_images(image_object_list,user_id)
 
+    # Print out dictionary for debugging
+    logging.info("Image ID and URL map dictionary:")
+    logging.info(image_id_url_map)
+
     # Copy over images to permanent blob store and save URLs in a list
     permanent_url_list = []
+    update_urls_dictionary = {}
     for key, value in image_id_url_map.items():
-        file_extension = os.path.splitext(value)[1]
-        permanent_storage_path = DESTINATION_DIRECTORY + "/" + key + file_extension
-        copyfile(value, permanent_storage_path)
+        image_url = key
+        image_id = value
+        logging.info("Original image URL: " + image_url)
+        logging.info("Image ID: " + str(image_id))
+        file_extension = os.path.splitext(image_url)[1]
+        logging.info("File extension: " + file_extension)
+        permanent_storage_path = DESTINATION_DIRECTORY + "/" + str(image_id) + file_extension
+        logging.info("Permanent storage destination path: " + permanent_storage_path)
+        logging.info("Now copying file from temporary to permanent storage...")
+        copyfile(image_url, permanent_storage_path)
+        logging.info("Done.")
         # Add image to the list of images to be returned in the response
         permanent_url_list.append(permanent_storage_path)
-        # Update the value in the dict to the new permanent storage URL
-        image_id_url_map[key] = permanent_storage_path
+        # Add ImageId and permanent storage url to new dictionary to be sent to update function
+        update_urls_dictionary[image_id] = permanent_storage_path
 
     logging.info("Done copying images to permanent blob storage.")
 
     logging.info("Now updating permanent URLs in the DB...")
-    data_access.update_image_urls(image_id_url_map, user_id)
+    data_access.update_image_urls(update_urls_dictionary, user_id)
     logging.info("Done.")
 
     # Construct response string of permanent URLs
